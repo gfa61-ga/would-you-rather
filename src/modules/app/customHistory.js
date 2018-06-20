7a84061415ba2424568255b4473feae72fd00841
Paul Kögel <p.koegel@wessels-technologie.de>
2018-06-20 13:13:33 +0200

    Add basename to customHistory. Hopefully this will fix routung issues on Github pages.

--- src/modules/app/customHistory.js
+++ src/modules/app/customHistory.js
@@ -2,4 +2,4 @@ import createBrowserHistory from 'history/createBrowserHistory';
 
 // With a custom history we can use React Router's imperative API inside
 // action creators and components without `withRoute`.
-export default createBrowserHistory();
+export default createBrowserHistory({ basename: process.env.PUBLIC_URL });

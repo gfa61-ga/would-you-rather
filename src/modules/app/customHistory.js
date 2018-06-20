579a0285b8a97982cdb1e535c7d51558ba2ad20b
Paul Kögel <p.koegel@wessels-technologie.de>
2018-06-20 14:16:03 +0200

    package.json: update react-router-dom.

--- src/modules/app/customHistory.js
+++ src/modules/app/customHistory.js
@@ -2,4 +2,4 @@ import createBrowserHistory from 'history/createBrowserHistory';
 
 // With a custom history we can use React Router's imperative API inside
 // action creators and components without `withRoute`.
-export default createBrowserHistory({ basename: alert(JSON.stringify(process.env.PUBLIC_URL)) });
+export default createBrowserHistory({ basename: process.env.PUBLIC_URL });
